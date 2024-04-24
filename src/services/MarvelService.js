e7e28386aa9048feaec0c781a490aa9a971fdedf
Erkin <ercaprio@gmail.com>
2024-04-25 01:04:37 +0500

    all characters request to server

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,8 +14,9 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=209&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=209&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
