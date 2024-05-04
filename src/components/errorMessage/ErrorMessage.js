d67211215158d4153837acea7168459e05b9982e
Erkin <ercaprio@gmail.com>
2024-05-04 22:14:09 +0500

    good tone of the application (spinner, errors...)

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        // <img src={process.env.PUBLIC_URL + '/error.gif'} alt="error" />
+        <img style={
+                {
+                    display: 'block',
+                    width: '250px',
+                    height: '250px',
+                    objectFit: 'contain',
+                    margin: '0 auto'
+                }
+            } 
+            src={img} 
+            alt="error" 
+        />
+    )
+}
+
+export default ErrorMessage;
