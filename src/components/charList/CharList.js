ea94b7503ed4fcff4ff64e99dfa596057d9b07ee
Erkin <ercaprio@gmail.com>
2024-05-14 01:42:24 +0500

    made a request to the CharList server

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,55 @@
+/* eslint-disable no-unused-vars */
 import './charList.scss';
+
+import { Component } from 'react/cjs/react.production.min';
+import MarvelService from '../../services/MarvelService';
+
 import abyss from '../../resources/img/abyss.jpg';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+class CharList extends Component{
+    state = {
+        chars: []
+    };
+
+    marvelService = new MarvelService();
+
+    onCharsLoaded = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(res => {
+                const chars = res.map((char, id) => {
+                    const {name, thumbnail, ...itemProps} = char;
+                    return (
+                        <li className="char__item" key={id}>
+                            <img src={thumbnail} 
+                                alt="thumbnail"
+                                style={{objectFit: thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'contain' : 'cover'}}
+                            />
+                            <div className="char__name">{name}</div>
+                        </li>
+                    )
+                })
+                this.setState({ chars });
+            })
+            .catch(error => console.error('Error loading characters:', error));
+    }
+
+    componentDidMount () {
+        this.onCharsLoaded();
+    }
+
+    render () {
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {this.state.chars}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
 }
 
 export default CharList;
